d71be9061efb2a2c6d33f25cfb1526529ed64a70
Matěj Koubík <mkoubik@gmail.com>
2010-05-13 12:04:49 +0200

    spec: Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be unfinished by default" do
+    task = Task.new(:title => "An unfinished task")
+    task.should_not be_finished
+  end
+
+  it "should be finishable" do
+    task = Task.new(:title => "A task")
+    task.finish
+    task.should be_finished
+  end
 end
